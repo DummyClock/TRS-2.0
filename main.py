5ff5674266d60c6d90526fbf54ce5f58889c68b1
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-30 21:48:58 -0400

    Changed way Service Key is loaded in

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from google.oauth2.service_account import Credentials
 from ss_manip_TRS import readReportFiles, readRequestFiles
 
 #from auth import SERVICE_KEY_JSON_FILE
-SERVICE_KEY_JSON_FILE = os.environ["SERVICE_KEY_JSON_FILE"]
+SERVICE_KEY_JSON_FILE = json.loads(os.environ["SERVICE_KEY_JSON_FILE"])
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/script.external_request', 'https://www.googleapis.com/auth/script.projects']
 creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
 client = gspread.authorize(creds)
-#path = os.path.dirname(os.path.realpath(__file__)) + '\\tmp_reports'
-#path2 = os.path.dirname(os.path.realpath(__file__)) + '\\tmp_requests'
 
 #Testing the functions. (Downloads files & lists names of downloaded files)
 listName = ["TRS (TEST): BOH Training Report".lower()]
