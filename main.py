81bb07cf1a21695ae8dcfcbdeb5db2db5373bc7b
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-30 21:52:27 -0400

    imported json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import gspread
+import json
 from jolt_scraper_v4 import downloadCSVs
 from google.oauth2.service_account import Credentials
 from ss_manip_TRS import readReportFiles, readRequestFiles
