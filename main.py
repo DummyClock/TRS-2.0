fd32ac96f25f61f33c53238a9800b0592df7b986
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-08-01 02:42:58 -0400

    Added comments and expanded upon fail safe method main.py
    The fail safe method now includes a minute wait time & it will remove all downloaded files in order to prevent dupes being created

--- main.py
+++ main.py
@@ -8,35 +8,51 @@ from ss_manip_TRS import readReportFiles, readRequestFiles
 #from auth import SERVICE_KEY_JSON_FILE
 SERVICE_KEY_JSON_FILE = json.loads(os.environ["SERVICE_KEY_JSON_FILE"])
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/script.external_request', 'https://www.googleapis.com/auth/script.projects']
-creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
-client = gspread.authorize(creds)
+# Removes all files from a directory
+def clearDirectory(path):
+    print("Clearing the directory " + path)
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        os.remove(file_path)
 
-#Testing the functions. (Downloads files & lists names of downloaded files)
-listName = ["TRS (TEST): BOH Training Report".lower()]
-listName2 = ["TRS (TEST): Request Training/Retraining (BOH)".lower()]
+# Entry Point
+if __name__ == "__main__":
+    # Set up credentials
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/script.external_request', 'https://www.googleapis.com/auth/script.projects']
+    creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
+    client = gspread.authorize(creds)
 
-#Download files
-attemps = 0
-maxAttempts = 3
-noeErorOccured = True
-while errorOccured and attemps < maxAttempts:
-    try:
-        errorOccured = False
-        paths = downloadCSVs(listName, listName2)
-    except WebDriverException as e:
-        print("Connection error occured. Reattempting to launch")
-        errorOccured = True
-        attemps += 1
 
-# Check if downloadCSV (part 1) was successful
-if os.path.exists(paths[0]):
-    readReportFiles(paths[0], client)
-else:
-    print("Unable to find " + str(paths[0]))
+    #Store the desired list names to download
+    listName = ["TRS (TEST): BOH Training Report".lower()]
+    listName2 = ["TRS (TEST): Request Training/Retraining (BOH)".lower()]
 
-# Check if downloadCSV (part 2) was successful
-if os.path.exists(paths[1]):
-    readRequestFiles(paths[1], client)
-else:
-    print("Unable to find " + str(paths[1]))
+    # Download files with desired names; will reattempt 3x if a connection error occurs
+    attemps = 0
+    maxAttempts = 3
+    errorOccured = True
+    while errorOccured and attemps < maxAttempts:
+        try:
+            errorOccured = False
+            paths = downloadCSVs(listName, listName2)
+        except WebDriverException as e:
+            print("Connection error occured. Reattempting to launch in a minute...")
+            errorOccured = True
+            attemps += 1
+            p1 = os.path.dirname(os.path.realpath(__file__)) + '\\tmp_reports'
+            p2 = os.path.dirname(os.path.realpath(__file__)) + '\\tmp_requests'
+            clearDirectory(p1)   # Delete temp files; attempt to redownload after the wait time
+            clearDirectory(p2)
+            time.sleep(60)
+
+    # Check if downloadCSV (part 1) was successful
+    if os.path.exists(paths[0]):
+        readReportFiles(paths[0], client)
+    else:
+        print("Unable to find " + str(paths[0]))
+
+    # Check if downloadCSV (part 2) was successful
+    if os.path.exists(paths[1]):
+        readRequestFiles(paths[1], client)
+    else:
+        print("Unable to find " + str(paths[1]))
